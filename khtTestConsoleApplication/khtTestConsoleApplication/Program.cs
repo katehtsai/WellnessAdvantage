254997af11328cb04af1f27c391c13cf4003b6a2
katehtsai <kate.tsai@fadv.com>
2016-10-11 11:41:05 -0600

    Add files via upload

---
+++ khtTestConsoleApplication/khtTestConsoleApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace khtTestConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is a test with GIT source control");
+            Console.WriteLine("this is 3rd line, testing merge, with GIT source control");
+			Console.WriteLine("this is another version with GIT source control");            
+            Console.WriteLine("this is new last line with GIT source control");
+
+            int a = 1;
+            int b = 2;
+            int c = b - a;
+        }
+
+        public void Method1(int x)
+        {
+            
+        }
+
+        public void Method2(string y)
+        { }
+    }
+}
